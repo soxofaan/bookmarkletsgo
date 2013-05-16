3ebc4d2b9b510f698879af336c5e4975593ba1db
Stefaan Lippens <soxofaan@gmail.com>
2013-05-17 00:16:54 +0200

    Added "fork me on github" ribbon

--- index.php
+++ index.php
@@ -33,6 +33,9 @@
             ?>
 
         </ul>
+
+        <a href="https://github.com/soxofaan/bookmarkletsgo"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+
     </div>
 
 </body>
